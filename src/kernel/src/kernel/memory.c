61ddd2792656da8988e552afb524d8e76f95b424
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-12-28 00:27:20 -0500

    Add logging to kernel memory

--- src/kernel/src/kernel/memory.c
+++ src/kernel/src/kernel/memory.c
@@ -5,19 +5,24 @@
 #include "libc/memory.h"
 #include "paging.h"
 
+#undef SERVICE
+#define SERVICE "KERNEL/MEMORY"
+
 static memory_t __memory;
 static size_t   __next_heap_page;
 
 void kmalloc_init(size_t next_heap_page) {
-    // KLOGS_DEBUG("kmalloc", "Init kmalloc to heap page 0x%X", next_heap_page);
+    KLOG_DEBUG("Init kmalloc to heap page 0x%X", next_heap_page);
 
     __next_heap_page = next_heap_page;
     memory_init(&__memory, kernel_alloc_page);
 
-    KLOGS_TRACE("kmalloc", "Override libc pmalloc functions");
+    KLOG_TRACE("Override libc pmalloc functions");
     _libc_config_malloc_call(kmalloc);
     _libc_config_realloc_call(krealloc);
     _libc_config_free_call(kfree);
+
+    KLOG_DEBUG("Initialized kernel memory management");
 }
 
 void * kmalloc(size_t size) {
@@ -34,20 +39,24 @@ void kfree(void * ptr) {
 
 void * kernel_alloc_page(size_t count) {
     if (!count) {
+        KLOG_ERROR("Tried to allocate 0 pages");
         return 0;
     }
 
     if (__next_heap_page + count >= MMU_DIR_SIZE * MMU_TABLE_SIZE) {
+        KLOG_ERROR("Tried to allocate past 4 GB");
         return 0;
     }
 
     mmu_dir_t * dir = paging_temp_map(VADDR_KERNEL_DIR);
 
     if (!dir) {
+        KLOG_ERROR("Could not create temporary map for mmu dir");
         return 0;
     }
 
     if (paging_add_pages(dir, __next_heap_page, __next_heap_page + count)) {
+        KLOG_ERROR("Failed to add %u pages starting at %u", count, __next_heap_page);
         paging_temp_free(VADDR_KERNEL_DIR);
         return 0;
     }
