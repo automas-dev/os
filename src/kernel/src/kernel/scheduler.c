842cf1213fc0e485d5726432675150e8056da3bc
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-03-19 17:29:21 -0400

    Update scheduler to use current task switch logic

--- src/kernel/src/kernel/scheduler.c
+++ src/kernel/src/kernel/scheduler.c
@@ -10,13 +10,22 @@
 static void idle();
 
 int scheduler_init(scheduler_t * scheduler, proc_man_t * pm) {
-    if (!scheduler || !pm) {
+    if (!scheduler) {
+        KLOG_ERROR("scheduler_init received a null pointer for the scheduler struct");
+        return -1;
+    }
+    if (!pm) {
+        KLOG_ERROR("scheduler_init received a null pointer for the process manager struct");
         return -1;
     }
 
+    KLOG_TRACE("Clearing scheduler struct");
     kmemset(scheduler, 0, sizeof(scheduler_t));
 
+    KLOG_TRACE("Useing process manager %p in scheduler %p", pm, scheduler);
     scheduler->pm = pm;
+
+    KLOG_TRACE("Finished init of scheduler %p", scheduler);
 
     return 0;
 }
@@ -24,61 +33,21 @@ int scheduler_init(scheduler_t * scheduler, proc_man_t * pm) {
 // TODO this is just a copy of the kernel / process manager current behavior
 int scheduler_run(scheduler_t * scheduler) {
     if (!scheduler) {
+        KLOG_ERROR("scheduler_run received a null pointer");
         return -1;
     }
 
-    if (cb_len(&get_kernel()->event_queue.queue) > 0) {
-        KLOG_DEBUG("There are %u events ready", cb_len(&get_kernel()->event_queue.queue));
-        ebus_event_t event;
-
-        if (cb_pop(&get_kernel()->event_queue.queue, &event) < 0) {
-            KPANIC("Failed to pop from event queue");
-        }
-
-        switch (event.event_id) {
-            case EBUS_EVENT_EXEC: {
-                int pid = kernel_exec(event.exec.filename, event.exec.argc, event.exec.argv);
-                if (pid > 0) {
-                    ebus_event_t proc_event  = {0};
-                    proc_event.event_id      = EBUS_EVENT_PROC_MADE;
-                    proc_event.proc_made.pid = pid;
-                    if (ebus_push(&get_kernel()->event_queue, &proc_event)) {
-                        KPANIC("Ebus push failed");
-                    }
-                }
-            } break;
-
-            case EBUS_EVENT_PROC_CLOSE: {
-                process_t * proc = kernel_find_pid(event.proc_close.pid);
-                if (!proc) {
-                    KPANIC("Failed to find pid");
-                }
-                if (pm_remove_proc(&get_kernel()->pm, proc->pid)) {
-                    KPANIC("Failed to remove process from pm");
-                }
-                process_free(proc);
-            } break;
-
-            default: {
-                if (pm_push_event(&get_kernel()->pm, &event)) {
-                    KPANIC("Failed to push event to process manager");
-                }
-            } break;
-        }
+    process_t * proc = get_active_task();
+    if (!proc) {
+        KPANIC("Failed to get active process");
     }
 
-    process_t * next = pm_get_next(scheduler->pm);
+    process_t * next = pm_get_next(kernel_get_proc_man());
+    KLOG_TRACE("Next after %u is %u in state %u", proc->pid, next->pid, next->state);
 
-    if (next) {
-        // TODO ebus events
-        process_resume(next, 0);
-        KPANIC("PROCESS SHOULD NOT RETURN TO SCHEDULER!");
+    if (pm_resume_process(kernel_get_proc_man(), next->pid)) {
+        KPANIC("Failed to resume process");
     }
-    else {
-        idle();
-    }
-}
 
-static void idle() {
-    asm("hlt");
+    KLOG_TRACE("Returned to process %u", proc->pid);
 }
