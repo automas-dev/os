f31b6bca54118accf2db70e41f7187cb1bb4aca8
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-04-06 07:25:14 -0400

    Cleanup init

--- src/apps/init/src/init.c
+++ src/apps/init/src/init.c
@@ -4,55 +4,14 @@
 #include "libc/stdio.h"
 
 void init() {
-    int pid = getpid();
-    printf("Hello World from Init, pid %d (u%u 0x%X)\n", pid, pid, pid);
+    // TODO why doesn't keyboard work without this?
     yield();
-    pid = getpid();
-    printf("Welcome back! My PID is %d (u%u 0x%X)\n", pid, pid, pid);
-
-    file_t * f = file_open("data/a.txt", "r");
-    printf("File pointer is %p\n", f);
-
-    if (f) {
-        file_seek(f, 0, FILE_SEEK_ORIGIN_END);
-        size_t file_size = file_tell(f);
-        printf("File a.txt has size %u\n", file_size);
-        file_seek(f, 0, FILE_SEEK_ORIGIN_START);
-
-        char * buff  = pmalloc(file_size);
-        size_t o_len = file_read(f, 1, file_size, buff);
-
-        printf("Read %u bytes\n", o_len);
-
-        printf("Message is ");
-        puts(buff);
-
-        file_close(f);
-    }
-
-    printf("Opening new process\n");
-
-    // char * filename = "foo";
-    // int    new_pid  = proc_open(filename, 0, 0);
-
-    // printf("New PID is %u\n", new_pid);
-
-    // new_pid = proc_open("demo", 0, 0);
-    // printf("New PID is %u\n", new_pid);
-
-    // proc_open("ping", 0, 0);
-    // proc_open("pong", 0, 0);
 
     int shell_pid = proc_open("shell", 0, 0);
-    // printf("Shell got pid %d\n", shell_pid);
     proc_set_foreground(shell_pid);
 
     proc_wait_pid(shell_pid, 0);
     printf("Shell exited\n");
-
-    // for (;;) {
-    //     yield();
-    // }
 }
 
 void __start() {
