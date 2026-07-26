d12a21b6f3414ae9a2871268a64d3fa71ee04bd7
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-26 11:49:29 -0400

    Fix header block for kernel/device/tar.h

--- src/kernel/include/kernel/device/tar.h
+++ src/kernel/include/kernel/device/tar.h
@@ -1,9 +1,9 @@
-#ifndef KERNEL_DEVICE_FS_FILE_H
-#define KERNEL_DEVICE_FS_FILE_H
+#ifndef KERNEL_DEVICE_TAR_H
+#define KERNEL_DEVICE_TAR_H
 
 #include "kernel/device/io_fs.h"
 
 io_fs_t * io_fs_tar_open(io_device_t * device);
 void      io_fs_tar_close(io_fs_t * fs);
 
-#endif // KERNEL_DEVICE_FS_FILE_H
+#endif // KERNEL_DEVICE_TAR_H
