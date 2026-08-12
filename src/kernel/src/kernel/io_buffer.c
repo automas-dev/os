038ab12e19099f3953d2e1be6a799a593c068872
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-08-12 03:45:19 -0400

    Rename io files and add logging

--- src/kernel/src/kernel/io_buffer.c
+++ src/kernel/src/kernel/io_buffer.c
@@ -1,24 +1,32 @@
+#define KLOG_SERVICE "IO_DEVICE"
+
 #include "kernel/io_buffer.h"
 
+#include "kernel/logs.h"
 #include "libc/memory.h"
 
 io_buffer_t * io_buffer_create(size_t size) {
     if (!size) {
+        KLOG_ERROR("io_buffer_create received a size of 0");
         return 0;
     }
 
     io_buffer_t * buff = pmalloc(sizeof(io_buffer_t));
     if (buff) {
         if (cb_create(&buff->buff, size, 1)) {
+            KLOG_ERROR("io_buffer_create failed to create circular buffer of size %u", size);
             pfree(buff);
             return 0;
         }
+        KLOG_TRACE("Created io buffer %p of size %u", buff, size);
     }
+
     return buff;
 }
 
 void io_buffer_free(io_buffer_t * buff) {
     if (!buff) {
+        KLOG_ERROR("io_buffer_free received a null pointer for the buffer struct");
         return;
     }
 
@@ -28,6 +36,7 @@ void io_buffer_free(io_buffer_t * buff) {
 
 int io_buffer_push(io_buffer_t * buff, char c) {
     if (!buff) {
+        KLOG_ERROR("io_buffer_push received a null pointer for the buffer struct");
         return -1;
     }
 
@@ -35,11 +44,13 @@ int io_buffer_push(io_buffer_t * buff, char c) {
 
     if (cb_len(cb) == cb_buff_size(cb)) {
         if (cb_pop(cb, 0)) {
+            KLOG_ERROR("Failed to pop element 0 from circular buffer %p of io buffer %p", cb, buff);
             return -1;
         }
     }
 
     if (cb_push(cb, &c)) {
+        KLOG_ERROR("Failed to push character 0x%02X (%c) to circular buffer %p of io buffer %p", c, c, cb, buff);
         return -1;
     }
 
@@ -48,12 +59,15 @@ int io_buffer_push(io_buffer_t * buff, char c) {
 
 int io_buffer_pop(io_buffer_t * buff, char * c_out) {
     if (!buff) {
+        KLOG_ERROR("io_buffer_pop received a null pointer for the buffer struct");
         return -1;
     }
+    // c_out can be null
 
     cb_t * cb = &buff->buff;
 
     if (cb_len(cb) == 0) {
+        KLOG_ERROR("Tried to pop char from empty to circular buffer %p of io buffer %p", cb, buff);
         return -1;
     }
 
@@ -62,6 +76,7 @@ int io_buffer_pop(io_buffer_t * buff, char * c_out) {
 
 size_t io_buffer_size(const io_buffer_t * buff) {
     if (!buff) {
+        KLOG_ERROR("io_buffer_size received a null pointer for the buffer struct");
         return 0;
     }
 
@@ -70,6 +85,7 @@ size_t io_buffer_size(const io_buffer_t * buff) {
 
 size_t io_buffer_length(const io_buffer_t * buff) {
     if (!buff) {
+        KLOG_ERROR("io_buffer_length received a null pointer for the buffer struct");
         return 0;
     }
 
---
+++ src/kernel/src/kernel/io_device.c
@@ -0,0 +1,50 @@
+#define KLOG_SERVICE "IO_DEVICE"
+
+#include "kernel/io_device.h"
+
+#include "kernel/logs.h"
+
+// TODO should null ptr checks be here or in each read function?
+size_t io_device_read(io_device_t * dev, char * buff, size_t count, size_t pos) {
+    if (!dev) {
+        KLOG_ERROR("io_device_read received a null pointer for the device struct");
+        return 0;
+    }
+    if (!buff) {
+        KLOG_ERROR("io_device_read received a null pointer for the buffer");
+        return 0;
+    }
+
+    return dev->read_fn(dev->device_data, buff, count, pos);
+}
+
+size_t io_device_write(io_device_t * dev, const char * buff, size_t count, size_t pos) {
+    if (!dev) {
+        KLOG_ERROR("io_device_write received a null pointer for the device struct");
+        return 0;
+    }
+    if (!buff) {
+        KLOG_ERROR("io_device_write received a null pointer for the buffer");
+        return 0;
+    }
+
+    return dev->write_fn(dev->device_data, buff, count, pos);
+}
+
+size_t io_device_size(io_device_t * dev) {
+    if (!dev) {
+        KLOG_ERROR("io_device_size received a null pointer for the device struct");
+        return 0;
+    }
+
+    return dev->size_fn(dev->device_data);
+}
+
+int io_device_close(io_device_t * dev) {
+    if (!dev) {
+        KLOG_ERROR("io_device_close received a null pointer for the device struct");
+        return -1;
+    }
+
+    return dev->close_fn(dev->device_data);
+}
