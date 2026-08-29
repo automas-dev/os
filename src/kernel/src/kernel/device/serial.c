7ee357776f9482088792e0703d4b2afc87a81eca
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-08-29 12:08:23 -0400

    Remove unused functions

--- src/kernel/src/kernel/device/serial.c
+++ src/kernel/src/kernel/device/serial.c
@@ -5,9 +5,7 @@
 #include "libc/string.h"
 
 static int    _serial_close(void * device_data);
-static size_t _serial_read(void * device_data, char * buff, size_t size, size_t pos);
 static size_t _serial_write(void * device_data, const char * buff, size_t size, size_t pos);
-static size_t _serial_size(void * device_data);
 
 io_device_t * io_device_serial_open() {
     io_device_t * dev = kmalloc(sizeof(io_device_t));
@@ -17,10 +15,7 @@ io_device_t * io_device_serial_open() {
         dev->flags = IO_DEVICE_FLAG_WRITE;
 
         dev->close_fn = _serial_close;
-
-        dev->read_fn  = _serial_read;
         dev->write_fn = _serial_write;
-        dev->size_fn  = _serial_size;
     }
     return dev;
 }
@@ -31,22 +26,15 @@ static int _serial_close(void * device_data) {
 }
 
 // device_data and pos not used
-static size_t _serial_read(void * device_data, char * buff, size_t size, size_t pos) {
-    return 0;
-}
-
-// device_data and pos not used
 static size_t _serial_write(void * device_data, const char * buff, size_t size, size_t pos) {
+    // TODO get a return value from driver
     serial_write(SERIAL_PORT_COM1, buff, size);
     return size;
 }
 
 // handle and pos not used
 int io_device_serial_write_raw(int handle, const char * buff, size_t size, size_t pos) {
+    // TODO get a return value from driver
     serial_write(SERIAL_PORT_COM1, buff, size);
     return size;
-}
-
-static size_t _serial_size(void * device_data) {
-    return 0;
 }
