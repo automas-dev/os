d53fe4445e957ec52780f29f4c4b04d8024701d5
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-08-29 11:15:19 -0400

    Don't call io_device read / write / size functions if flag not set, check for missing function pointer

--- src/kernel/src/kernel/io_device.c
+++ src/kernel/src/kernel/io_device.c
@@ -3,10 +3,19 @@
 #include "kernel/io/device.h"
 #include "kernel/logs.h"
 
-// TODO should null ptr checks be here or in each read function?
 size_t io_device_read(io_device_t * dev, char * buff, size_t count, size_t pos) {
     if (!dev) {
         KLOG_WARNING("io_device_read received a null pointer for the device struct");
+        return 0;
+    }
+
+    if (!(dev->flags & IO_DEVICE_FLAG_READ)) {
+        KLOG_WARNING("io_device_read called on device without read flag");
+        return 0;
+    }
+
+    if (!dev->read_fn) {
+        KLOG_WARNING("io_device_read called on device with no read function");
         return 0;
     }
 
@@ -19,12 +28,32 @@ size_t io_device_write(io_device_t * dev, const char * buff, size_t count, size_
         return 0;
     }
 
+    if (!(dev->flags & IO_DEVICE_FLAG_WRITE)) {
+        KLOG_WARNING("io_device_write called on device without write flag");
+        return 0;
+    }
+
+    if (!dev->write_fn) {
+        KLOG_WARNING("io_device_write called on device with no write function");
+        return 0;
+    }
+
     return dev->write_fn(dev->device_data, buff, count, pos);
 }
 
 size_t io_device_size(io_device_t * dev) {
     if (!dev) {
         KLOG_WARNING("io_device_size received a null pointer for the device struct");
+        return 0;
+    }
+
+    if (!(dev->flags & IO_DEVICE_FLAG_SIZED)) {
+        KLOG_WARNING("io_device_size called on device without sized flag");
+        return 0;
+    }
+
+    if (!dev->size_fn) {
+        KLOG_WARNING("io_device_size called on device with no size function");
         return 0;
     }
 
@@ -35,6 +64,11 @@ int io_device_close(io_device_t * dev) {
     if (!dev) {
         KLOG_WARNING("io_device_close received a null pointer for the device struct");
         return -1;
+    }
+
+    if (!dev->close_fn) {
+        KLOG_WARNING("io_device_close called on device with no close function");
+        return 0;
     }
 
     return dev->close_fn(dev->device_data);
