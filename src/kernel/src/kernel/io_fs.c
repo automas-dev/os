0063eee78cea30038189cd25187356a834efc544
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-08-29 11:17:57 -0400

    Add checks for missing function pointers

--- src/kernel/src/kernel/io_fs.c
+++ src/kernel/src/kernel/io_fs.c
@@ -9,6 +9,11 @@ io_fs_file_t * io_fs_file_open(io_fs_t * fs, const char * path, const char * mod
         return 0;
     }
 
+    if (!fs->file_open_fn) {
+        KLOG_WARNING("io_fs_file_open called on filesystem with missing required file open function");
+        return 0;
+    }
+
     return fs->file_open_fn(fs->fs_data, path, mode);
 }
 
@@ -16,6 +21,11 @@ int io_fs_file_close(io_fs_file_t * file) {
     if (!file) {
         KLOG_WARNING("io_fs_file_close received a null pointer for file struct");
         return -1;
+    }
+
+    if (!file->file_close_fn) {
+        KLOG_WARNING("io_fs_file_close called on file with missing required close function");
+        return 0;
     }
 
     return file->file_close_fn(file->fs_data, file->file_data);
@@ -27,6 +37,11 @@ int io_fs_file_stat(io_fs_t * fs, const char * path, io_fs_stat_t * stat_out) {
         return -1;
     }
 
+    if (!fs->file_stat_fn) {
+        KLOG_WARNING("io_fs_file_stat called on filesystem with missing required file stat function");
+        return 0;
+    }
+
     return fs->file_stat_fn(fs->fs_data, path, stat_out);
 }
 
@@ -36,9 +51,13 @@ size_t io_fs_file_read(io_fs_file_t * file, char * buff, size_t count) {
         return 0;
     }
 
-    // TODO include pointer
     if (!(file->flags & IO_FS_FLAG_READ)) {
         KLOG_WARNING("io_fs_file_read called on file without read flag");
+        return 0;
+    }
+
+    if (!file->file_read_fn) {
+        KLOG_WARNING("io_fs_file_read called on file with no read function");
         return 0;
     }
 
@@ -51,9 +70,13 @@ size_t io_fs_file_write(io_fs_file_t * file, const char * buff, size_t count) {
         return 0;
     }
 
-    // TODO include pointer
     if (!(file->flags & IO_FS_FLAG_WRITE)) {
         KLOG_WARNING("io_fs_file_write called on file without write flag");
+        return 0;
+    }
+
+    if (!file->file_write_fn) {
+        KLOG_WARNING("io_fs_file_write called on file with no write function");
         return 0;
     }
 
@@ -66,6 +89,11 @@ int io_fs_file_seek(io_fs_file_t * file, int offset, int origin) {
         return -1;
     }
 
+    if (!file->file_seek_fn) {
+        KLOG_WARNING("io_fs_file_seek called on file with missing required seek function");
+        return 0;
+    }
+
     return file->file_seek_fn(file->fs_data, file->file_data, offset, origin);
 }
 
@@ -73,6 +101,11 @@ int io_fs_file_tell(io_fs_file_t * file) {
     if (!file) {
         KLOG_WARNING("io_fs_file_tell received a null pointer for file");
         return -1;
+    }
+
+    if (!file->file_tell_fn) {
+        KLOG_WARNING("io_fs_file_tell called on file with missing required tell function");
+        return 0;
     }
 
     return file->file_tell_fn(file->fs_data, file->file_data);
