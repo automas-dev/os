f68cc7422737f6122aa3357f95a0de5d85b3d817
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-09-01 05:29:30 -0400

    Fix send_call and send_call_noret clobbering ebx

--- src/libk/src/calls.asm
+++ src/libk/src/calls.asm
@@ -5,10 +5,12 @@ global send_call
 
 send_call_noret:
 send_call:
-    mov eax, [esp+4]
+    push ebx
+    mov eax, [esp+8]
     mov ebx, esp
-    add ebx, 8
+    add ebx, 12
 
     int 48
 
+    pop ebx
     ret
